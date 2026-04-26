e9f011c848631ef59698dac1192183f5e577b4bd
Yasmeen hani <yasmeen.hani@outlook.com>
2026-04-26 14:30:24 +0300

    Refactor StoreUsers to handle Users only
    Refactor StoreUsers class to manage Users only, removing Member and Employee handling.

--- src/Main/StoreUsers.java
+++ src/Main/StoreUsers.java
@@ -4,34 +4,18 @@ import java.util.*;
 // We will add a method that is called for every update (also for when any alter is done called "save" and we will have the save / load in the place)
 public class StoreUsers {
     
-    static ArrayList<Member> members = new ArrayList<Member>();
-    static ArrayList<Employee> employees = new ArrayList<Employee>();
     static ArrayList<Users> users = new ArrayList<Users>();
 
-    public ArrayList<Member> getMembers() {
-        return members;
+    public ArrayList<Member> getUsers() {
+        return users;
     }
 
-    public ArrayList<Employee> getEmployees() {
-        return employees;
+    public void addEmployee(User user){
+        users.add(user);
     }
     
-    public void addEmployee(Employee employee){
-    employees.add(employee);
-    users.add(employee);
-    }
-    
-    public void addMember(Member member){
-    members.add(member);
-    users.add(member);
-    }
-    
-    public void removeMember(Member member){
-    members.remove(member);
-    }
-    
-    public void removeEmployee(Employee employee){
-    employees.remove(employee);
+    public void removeEmployee(User user){
+        users.add(user);
     }
     
     
