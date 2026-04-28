96dfe3ef537298136613c65c0299f0f2253c01e4
Yasmeen hani <yasmeen.hani@outlook.com>
2026-04-28 10:58:10 +0300

    Update PolyStudent.java

--- src/Main/PolyStudent.java
+++ src/Main/PolyStudent.java
@@ -2,6 +2,9 @@ package Main;
 
 public class PolyStudent extends Member{
     double studentDiscount;
+    String course;
+    boolean Inteam;
+    String Teams;
     
     PolyStudent(){
         studentDiscount = 0.0;
