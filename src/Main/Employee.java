f763d7304d1722a1afdedec02845f853188ff543
marvalh <marwaalhoda11@gmail.com>
2026-05-28 17:19:29 +0300

    added salary getter and setter for alter page

--- src/Main/Employee.java
+++ src/Main/Employee.java
@@ -1,16 +1,23 @@
 package Main;
+
 import java.io.Serializable;
 import java.util.*;
 
-public class Employee extends Users implements Serializable{
-    
-   private int staffID;
-   private double salary;
+public class Employee extends Users implements Serializable {
 
+    private int staffID;
+    private double salary;
 
     public int getEmpID() {
         return this.userID;
     }
 
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
 }
